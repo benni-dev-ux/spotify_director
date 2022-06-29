1f9d2d0a6e1c8f4fc746eb28847b1b9419885447
Benedikt <benediktsteinmueller@googlemail.com>
2022-06-29 22:50:42 +0200

    cleanup

--- director.py
+++ director.py
@@ -5,19 +5,15 @@ import json
 scope = "playlist-modify-public"
 username = "bennisteinmueller"
 
+discover_weekly_ID = "37i9dQZEVXcDYET2LCFeAo"
+discover_weekly_archive_ID = "2tPmW96s1cZK1OLNAJIwEY"
+
 token = SpotifyOAuth(scope=scope, username=username)
 spotifyObject = spotipy.Spotify(auth_manager=token)
 
-plname = input("Enter name")
-
-
-# playlists = spotifyObject.user_playlists(username)
-# trackids = playlists["items"][0]["id"]
-# print(trackids)
-
 
 tracks = spotifyObject.playlist_tracks(
-    "37i9dQZEVXcDYET2LCFeAo",
+    discover_weekly_ID,
     fields=None,
     limit=100,
     offset=0,
@@ -31,6 +27,4 @@ for i in range(30):
     trackids.append(tracks["items"][i]["track"]["uri"])
 
 
-spotifyObject.playlist_add_items("2tPmW96s1cZK1OLNAJIwEY", trackids, position=None)
-
-# 2tPmW96s1cZK1OLNAJIwEY
+spotifyObject.playlist_add_items(discover_weekly_archive_ID, trackids, position=None)
