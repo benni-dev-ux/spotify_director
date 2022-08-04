053cb3d07e44c768e789d4bf32a41be77e74fcec
Benedikt <benediktsteinmueller@googlemail.com>
2022-08-04 13:58:26 +0200

    added gui and environment

--- director.py
+++ director.py
@@ -1,34 +1,79 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-scope = "playlist-modify-public"
-username = "bennisteinmueller"
 
-discover_weekly_ID = "37i9dQZEVXcDYET2LCFeAo"
-discover_weekly_archive_ID = "1jAXcKJmLvHxk9OpMofCEI"
+import tkinter as tk
+from tkinter import ttk
+import sv_ttk
 
-token = SpotifyOAuth(scope=scope, username=username)
-spotifyObject = spotipy.Spotify(auth_manager=token)
 
-tracks = spotifyObject.playlist_items(
-    discover_weekly_ID,
-    fields=None,
-    limit=100,
-    offset=0,
-    market=None,
-    additional_types=("track", "episode"),
-)
+class SpotifyDirectorGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Spotify Discover Weekly Archiver")
+        master.resizable(False, False)
 
-trackids = []
+        sv_ttk.use_light_theme()  # Set light theme
 
-length = len(tracks["items"])
+        self.label = ttk.Label(
+            master, text="Spotify Discover Weekly Archiver", font="Helvetica 16 bold "
+        )
 
-for i in range(length):
-    trackids.append(tracks["items"][i]["track"]["uri"])
+        self.text = ttk.Label(
+            master,
+            text="Adds all songs from Discover Weekly to your Discover Weekly Archive",
+            font="Helvetica 10 ",
+        )
 
-spotifyObject.playlist_add_items(discover_weekly_archive_ID, trackids, position=None)
+        self.close_button = ttk.Button(master, text="Close", command=master.quit)
+        self.add_button = ttk.Button(
+            master, text="Add Songs", command=self.add_songs_to_archive
+        )
+        self.add_button.focus()
 
-print("Added new songs to your Discover Weekly Archive")
+        self.label.grid(columnspan=2, row=0, sticky=tk.W, padx=30, pady=15)
+        self.text.grid(columnspan=2, row=1, sticky=tk.W, padx=30, pady=10)
+        self.close_button.grid(row=2, column=0, padx=30, pady=20, sticky=tk.EW)
+        self.add_button.grid(row=2, column=1, padx=30, pady=20, sticky=tk.EW)
+
+    def add_songs_to_archive(self):
+        scope = "playlist-modify-public"
+        username = "bennisteinmueller"
+
+        discover_weekly_ID = "37i9dQZEVXcDYET2LCFeAo"
+        discover_weekly_archive_ID = "1jAXcKJmLvHxk9OpMofCEI"
+        token = SpotifyOAuth(scope=scope, username=username)
+        spotifyObject = spotipy.Spotify(auth_manager=token)
+
+        tracks = spotifyObject.playlist_items(
+            discover_weekly_ID,
+            fields=None,
+            limit=100,
+            offset=0,
+            market=None,
+            additional_types=("track", "episode"),
+        )
+
+        trackids = []
+
+        length = len(tracks["items"])
+
+        for i in range(length):
+            trackids.append(tracks["items"][i]["track"]["uri"])
+
+        spotifyObject.playlist_add_items(
+            discover_weekly_archive_ID, trackids, position=None
+        )
+
+        print("Added new songs to your Discover Weekly Archive")
+
+
+root = tk.Tk()
+
+
+SD_GUI = SpotifyDirectorGUI(root)
+root.mainloop()
+
 
 # Create Cover and updating cover image
 
